cd41d297c88198321fb71431bbc050b292f87a64
baranulu <barann1461@hotmail.com>
2020-08-18 00:15:38 +0300

    ödevler

---
+++ wfa_ödev_extension_metot/wfa_ödev_extension_metot/Class2.cs
@@ -0,0 +1,25 @@
+﻿
+namespace wfa_ödev_extension_metot
+{
+  public static class Class2
+    {
+        public static int Exmethot(this string str)
+        {
+            return int.Parse (str.ToLower()
+                .Replace('ö', 'o')
+                 .Replace('ü', 'u')
+                 .Replace('ı', 'i')
+                 .Replace('ğ', 'g')
+                 .Replace('ç', 'c')
+                 .Replace('ş', 's')
+                 .Replace('ç', 'c'));
+        }
+        static void main (string[] args)
+        {
+            string str1 = "ÖYKÜ";
+            int n = str1.Exmethot();
+            System.Windows.Forms.MessageBox.Show("Result: {0}", n.ToString());
+
+        }
+    }
+}
---
+++ wfa_ödev_extension_metot/wfa_ödev_extension_metot/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfa_ödev_extension_metot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class2.Exmethot("ÖYKÜ");
+        }
+    }
+}
